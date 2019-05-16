09c394a0bea8117b9cbfc6ded973507b26934d09
j4ckstraw <j4ckstraw@foxmail.com>
2019-05-16 20:03:02 +0800

    Add show-branch

--- conf.sh
+++ conf.sh
@@ -2,4 +2,6 @@
 
 git config --global alias.st status
 git config --global alias.co checkout
+git config --global alias.ci commit 
 git config --global alias.lg "log --oneline --decorate --graph"
+git config --global alias.sh show-branch 
